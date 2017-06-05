fd8c07849e3c84f3c42944e9d871f76c7655b3ac
Andrew <Andrew@Andrew>
2017-06-05 08:34:45 -0400

    change my test 2

--- test/src/test/temp.java
+++ test/src/test/temp.java
@@ -5,7 +5,7 @@ public class temp {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Restart");
-		System.out.println("my test");
+		System.out.println("my test 2");
 	}
 
 }
